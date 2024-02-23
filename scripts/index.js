71b05dcf1a14ed2409755494174ddf69ddf66392
Artur <kotorinartur@gmail.com>
2024-02-23 14:48:48 +0300

    second try mesto

--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,23 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 const cardsList = document.querySelector('.places__list');
 
-initialCards.forEach(function(item) {
+function createCard(name, link) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true); 
+    
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__title').textContent = name;
 
-    cardElement.querySelector('.card__image').src = item.link;
-    cardElement.querySelector('.card__title').textContent = item.name;
-
-    const deleteButton = cardElement.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', deleteCard);
-
-    cardsList.append(cardElement);
-})
+    return cardElement;
+}
 
 function deleteCard(event) {
     const deleteListItem = event.target.closest('.places__item');
     deleteListItem.remove();
 }
+
+initialCards.forEach(function(item) {
+    cardElement = createCard(item.name, item.link);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteCard);
+    cardsList.append(cardElement);
+})
